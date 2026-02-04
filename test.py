239ce4004b832ec4891ac24d51347d2288b08380
Matvey1045866 <love.dad.matvey.butusov@mail.ru>
2026-02-04 16:35:01 +0300

    Add code validation logic in test.py
    Implement input validation for code entry

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Loading...")
+var = input("Enter a code: ")
+if var == "MT52626gshuUi636@github.com":
+    print("Pass")
+    exit()
+else:
+    raise ValueError("Code", var, "not passable!")
